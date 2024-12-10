f741ba8d2a47df77a9b190645b7824bfed19ee89
jaykchen@icloud.com <jaykchen@icloud.com>
2024-12-09 20:05:13 -0500

    template work?

--- flake.nix
+++ flake.nix
@@ -1,4 +1,6 @@
 {
+  description = "Rust Development Environment with WASM Support";
+
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
     rust-overlay = {
@@ -7,45 +9,77 @@
     };
   };
 
-  outputs = { self, nixpkgs, rust-overlay }:
+  outputs =
+    { self
+    , nixpkgs
+    , rust-overlay
+    }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ rust-overlay.overlays.default ];
         };
       });
+
     in
     {
-      templates.default = {
-        path = ./.;
-        description = "Rust development environment with WASM support";
-        welcomeText = ''
-          # Rust Development Environment
-          Run `nix develop` to enter the development shell.
-        '';
-      };
+      devShells = forAllSystems
+        ({ pkgs }: {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # Pin specific Rust version using rust-bin and add WASM targets
+              (rust-bin.stable.latest.default.override {
+                targets = [ "wasm32-wasip2" "wasm32-unknown-unknown" ];
+                extensions = [ "rust-src" "rustfmt" ];
+              })
 
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            (rust-bin.stable.latest.default.override {
-              targets = [ "wasm32-wasip2" "wasm32-unknown-unknown" ];
-              extensions = [ "rust-src" "rustfmt" ];
-            })
-            openssl
-            pkg-config
-            cargo-deny
-            cargo-edit
-            cargo-watch
-            rust-analyzer
-          ];
+              # Other useful Rust tools
+              cargo-make
+              cargo-edit
+              cargo-audit
+              cargo-deny
+              rust-analyzer
 
-          env = {
-            RUST_SRC_PATH = "${pkgs.rust-bin.stable.latest.default}/lib/rustlib/src/rust/library";
+              # General development tools
+              pkg-config
+              openssl
+            ];
+
+            # Set up rust-src for rust-analyzer
+            env = {
+              RUST_SRC_PATH = "${pkgs.rust-bin.stable.latest.default}/lib/rustlib/src/rust/library";
+            };
           };
+        });
+
+      templates = {
+        default = {
+          path = ./.;
+          description = "Rust Development Environment with WASM Support";
+          welcomeText = ''
+            # Rust Development Environment with WASM Support
+
+            This is your new Rust development environment, with support for compiling to WebAssembly (WASM) using the wasm32-wasip2 target.
+
+            To enter the development shell, run:
+
+              nix develop
+
+            You can now build and run your project:
+
+              cargo build
+              cargo run
+          '';
         };
-      });
+      };
     };
 }
